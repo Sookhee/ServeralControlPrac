72cc7d058062474f88d35d1b0b05487a3f59ccc6
Sookhee <s2018s38@e-mirim.hs.kr>
2020-07-21 10:14:25 +0900

    checkBox

--- ServeralControlPrac/Form1.Designer.cs
+++ ServeralControlPrac/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 38);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(71, 15);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- ServeralControlPrac/Form1.cs
+++ ServeralControlPrac/Form1.cs
@@ -15,6 +15,57 @@ namespace ServeralControlPrac
         public Form1()
         {
             InitializeComponent();
+            CheckBox cb1 = new CheckBox();
+            CheckBox cb2 = new CheckBox();
+            CheckBox cb3 = new CheckBox();
+            Button btn = new Button();
+
+            cb1.Text = "감자";
+            cb2.Text = "고구마";
+            cb3.Text = "토마토";
+            btn.Text = "클릭!";
+
+            cb1.Location = new Point(100, 10);
+            cb2.Location = new Point(100, 40);
+            cb3.Location = new Point(100, 70);
+            btn.Location = new Point(100, 100);
+
+            btn.Click += ButtonClick;
+            Controls.Add(cb1);
+            Controls.Add(cb2);
+            Controls.Add(cb3);
+            Controls.Add(btn);
+
+           
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+
+            foreach(var item in Controls)
+            {
+                if(item is CheckBox)
+                {
+                    CheckBox checkBox = (CheckBox)item;
+                    if (checkBox.Checked)
+                    {
+                        list.Add(checkBox.Text);
+                    }
+                }
+            }
+
+            MessageBox.Show(string.Join(", ", list));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://google.co.kr/");
         }
     }
 }
